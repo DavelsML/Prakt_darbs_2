708ade6a31831e6ca0c7f6bb5d68e1e81f833dc6
MainLubejs <lubejsdavis@gmail.com>
2024-10-22 09:31:57 +0300

    Corrected code

--- database/migrations/2024_10_12_082821_create_delivery_table.php
+++ database/migrations/2024_10_12_082821_create_delivery_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('delivery', function (Blueprint $table) {
+        Schema::create('deliveries', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('description');
